17d9eff83f860766e84f4dd1880ed7fdf5b2d556
Jonathan Cooper <Jon@Jonathans-MacBook-Pro.local>
2015-05-12 15:05:58 -0400

    adding in old portfolio

---
+++ JonathanCooperD.github.io/README.md
@@ -0,0 +1,3 @@
+portfolio
+=========
+# CoopAtCanvas.github.io
---
+++ JonathanCooperD.github.io/js/controllers.js
@@ -0,0 +1,34 @@
+var app = angular.module("app", []);
+
+app.config(function ($routeProvider){
+	$routeProvider.when("/",
+		{
+			templateUrl: "partials/home.html",
+			controller: "AppCtrl"
+		}
+	).when("/cutty-sark",
+		{
+			templateUrl: "partials/cutty-sark.html",
+			controller: "AppCtrl"
+		}
+	).when("/nintendo", 
+		{ 
+			templateUrl: "partials/nintendo.html",
+			controller: "AppCtrl"
+		}
+	).when("/live-bait", 
+		{
+			templateUrl: "partials/live-bait.html",
+			controller: "AppCtrl"
+		}
+	).when("/mario-party", 
+		{
+			templateUrl: "partials/mario.html",
+			controller: "AppCtrl"
+		}
+	).otherwise({redirectTo: '/'});
+})
+
+app.controller("AppCtrl", function($scope){
+	$scope.model = {}
+})
